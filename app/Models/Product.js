0aac0600be26804be02b75b528b8f3a77792d487
Rafael Amaral Bellandi <rafaelbellandi@gmail.com>
2020-11-07 12:06:52 -0400

    Product model

---
+++ app/Models/Product.js
@@ -0,0 +1,23 @@
+import Product from './Product';
+
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Product extends Model {
+  image() {
+    return this.belongsTo('App/Models/Image')
+}
+  images() {
+    return this.belongsToMany('App/Models/Image')
+  }
+  categories() {
+    return this.belongsToMany('App/Models/Category')
+  }
+  coupons() {
+    return this.belongsToMany('App/Models/Coupon')
+  }
+}
+
+module.exports = Product

9c27bde9a59eb9f3e2276eb3b928d6a8e2e0a060
Rafael Amaral Bellandi <rafaelbellandi@gmail.com>
2020-11-08 10:02:47 -0400

    Client Routes

--- start/routes/client.js
+++ start/routes/client.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 /*
 |--------------------------------------------------------------------------
@@ -14,14 +14,16 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+const Route = use('Route');
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-}).as('home')
+Route.group(() => {
+  Route.get('products', 'ProductController.index');
+  Route.get('products/:id', 'ProductController.show');
 
-require('./auth');
-
-require('./admin');
-
-require('./client');
+  Route.get('orders', 'OrderController.index');
+  Route.get('orders/:id', 'OrderController.show');
+  Route.post('orders', 'OrderController.store');
+  Route.put('orders/:id', 'OrderController.put');
+})
+  .prefix('v1')
+  .namespace('Client');

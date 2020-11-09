a50716aef1ca8961cc8aabff1a528f68c3dc550d
Rafael Amaral Bellandi <rafaelbellandi@gmail.com>
2020-11-09 08:29:58 -0400

    Manage uploads in helpers

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -3,7 +3,6 @@
 const crypto = use('crypto');
 const Helpers = use('Helpers');
 
-
 const str_random = async (length = 40) => {
   let string = '';
   let len = string.length;
@@ -18,10 +17,46 @@ const str_random = async (length = 40) => {
       .substr(0, size);
   }
 
-  return string
+  return string;
 };
 
+const manage_single_upload = async (file, path = null) => {
+  path ? path : Helpers.publicPath('uploads');
+
+  const random_name = await str_random(30);
+  let filename = `${new Date().getTime()}-${random_name}.${file.subtype()}`;
+
+  await file.move(path, {
+    name: filename,
+  });
+
+  return file;
+};
+
+const manage_multiple_uploads = async (fileJar, path = null) => {
+  path ? path : Helpers.publicPath('uploads');
+  let successes = [],
+    errors = [];
+
+  await Promise.all(
+    fileJar.files.map(async file => {
+      let random_name = await str_random(30);
+      let filename = `${new Date().getTime()}-${random_name}.${file.subtype()}`;
+      await file.move(path, {
+        name: filename,
+      });
+      if (file.moved()) {
+        successes.push(file);
+      } else {
+        errors.push(file.error());
+      }
+    })
+  );
+  return (successes, errors)
+};
 
 module.exports = {
-  str_random
-}
+  str_random,
+  manage_single_upload,
+  manage_multiple_uploads
+};

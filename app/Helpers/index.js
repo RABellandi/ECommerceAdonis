b1e1bc8e8a8a4afd1fffeb9ba33ff884164111c9
Rafael Amaral Bellandi <rafaelbellandi@gmail.com>
2020-11-09 08:00:45 -0400

    Correction of bugs in str_random helper

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -3,13 +3,14 @@
 const crypto = use('crypto');
 const Helpers = use('Helpers');
 
+
 const str_random = async (length = 40) => {
   let string = '';
   let len = string.length;
 
   if (len < length) {
     let size = length - len;
-    let bytes = await crypto.ramdomBytes(size);
+    let bytes = await crypto.randomBytes(size);
     let buffer = Buffer.from(bytes);
     string += buffer
       .toString('base64')

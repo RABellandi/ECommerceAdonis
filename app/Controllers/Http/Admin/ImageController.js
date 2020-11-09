2bded6d6c07a85f2e139f52a1a6c32a016455ce8
Rafael Amaral Bellandi <rafaelbellandi@gmail.com>
2020-11-09 10:05:26 -0400

    Upload controller half-complete

--- app/Controllers/Http/Admin/ImageController.js
+++ app/Controllers/Http/Admin/ImageController.js
@@ -1,8 +1,11 @@
-'use strict'
+'use strict';
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Image = use('App/Models/Image');
+const { manage_single_upload, manage_multiple_uploads } = use('App/Helpers');
 
 /**
  * Resourceful controller for interacting with images
@@ -17,7 +20,11 @@ class ImageController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index({ request, response, view }) {
+    const images = await Image.query()
+      .orderBy('id', 'DESC')
+      .paginate(pagination.page, pagination.limit);
+    return response.send(images);
   }
 
   /**
@@ -28,7 +35,52 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, response }) {
+    try {
+      //one upload
+      const fileJar = request.file('images', {
+        types: ['images'],
+        size: '2mb',
+      });
+      let images = [];
+      if (!fileJar.files) {
+        const file = await manage_single_upload(fileJar);
+        if (file.moved()) {
+          const image = await Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype,
+          });
+          images.push(image);
+          return response.status(201).send({ successes: images, errors: {} });
+        }
+        return response.status(400).send({
+          message: 'Não foi possível processar a imagem no momento!',
+        });
+      }
+
+      //multiples uploads
+      let files = await manage_multiple_uploads(fileJar);
+      await Promise.all(
+        file.successes.map(async file => {
+          const image = await Image.create({
+            path: file.fileName,
+            size: file.size,
+            orignal_name: file.clientName,
+            extension: file.subtype,
+          });
+          images.push(image);
+        })
+      );
+      return response
+        .status(201)
+        .send({ successes: images, errors: file.errors });
+    } catch (e) {
+      return response.status(400).send({
+        message: 'Não foi possível processar a imagem!',
+      });
+    }
   }
 
   /**
@@ -40,8 +92,7 @@ class ImageController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
+  async show({ params, request, response, view }) {}
 
   /**
    * Update image details.
@@ -51,8 +102,7 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
-  }
+  async update({ params, request, response }) {}
 
   /**
    * Delete a image with id.
@@ -62,8 +112,7 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
-  }
+  async destroy({ params, request, response }) {}
 }
 
-module.exports = ImageController
+module.exports = ImageController;
